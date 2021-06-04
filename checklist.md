84bc9a8df2e9532d8f16ff4d783443c0cade97eb
Brandon Wharton <whartonbm@gmail.com>
2021-06-04 18:26:44 -0500

    Fleshed out checklist and data model, planning done

---
+++ checklist.md
@@ -0,0 +1,63 @@
+To Do:
+
+- [ ] App.jsx
+    - [ ] GET
+        - [ ] Axios in App component
+        - [ ] galleryList state
+        - [ ] send props: galleryList, GET function
+
+- [ ] GalleryList
+    - [ ] Map through list, return GalleryItem calls
+        - [ ] send props: GET function, key, full image object
+
+- [ ] GalleryItem
+    - [ ] Display each image
+        - [ ] Make clickable
+            - [ ] On click, toggle discription
+    - [ ] Like button
+        - [ ] Connect to PUT request
+        - [ ] Display number of likes
+        - [ ] update using GET function
+
+    
+
+
+
+
+
+
+Data Model
+
+routes:
+
+/gallery -GET
+/gallery/like/id -PUT
+// maybe not necessary 
+/gallery/description/id -New PUT
+
+galleryItems = [
+        { 
+            id: Unique Number
+            path: '...../images/(picture).jpg', 
+            description: Description of image, 
+            likes: Number, 
+            title: Title of image
+            // maybe not necessary, play around with onClick and toggles first for simplicity
+            showDescription: default false, toggles on click to show description instead of image
+        }
+]
+
+components (minimum)
+
+App
+GalleryList - The loop
+GalleryItem - The display divs
+
+states
+
+galleryList - App.jsx - set by GET request
+likesTotal - GalleryItem - set by PUT
+// maybe not necessary
+showDescription - GalleryItem - set by conditional rendering/PUT
+
+
--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,11 +1,11 @@
 const galleryItems = [
-    { id: 1, path: 'images/acro.jpg', description: 'Acro description here', likes: 0, title: 'Acro Yoga' },
-    { id: 2, path: 'images/kauai.jpg', description: 'Kauai description here', likes: 0, title: 'Nepali Coast: Kauai, HI' },
-    { id: 3, path: 'images/mia.jpg', description: 'Mia description here', likes: 0, title: 'Mia' },
-    { id: 4, path: 'images/peaches.jpg', description: 'Peaches description here', likes: 0, title: 'Peaches' },
-    { id: 5, path: 'images/plitvice.jpg', description: 'Plitvice description here', likes: 0, title: 'Plitvice Lakes National Park' },
-    { id: 6, path: 'images/sacre-coeur.jpg', description: 'Sacre Coeur decription here', likes: 0, title: 'Sacré-Cœur Basilica' },
-    { id: 7, path: 'images/wedding.jpg', description: 'Wedding description here', likes: 0, title: 'Wedding' }
+    { id: 1, path: 'images/acro.jpg', description: 'Acro description here', likes: 0, title: 'Acro Yoga', showDescription: false },
+    { id: 2, path: 'images/kauai.jpg', description: 'Kauai description here', likes: 0, title: 'Nepali Coast: Kauai, HI', showDescription: false  },
+    { id: 3, path: 'images/mia.jpg', description: 'Mia description here', likes: 0, title: 'Mia', showDescription: false  },
+    { id: 4, path: 'images/peaches.jpg', description: 'Peaches description here', likes: 0, title: 'Peaches', showDescription: false  },
+    { id: 5, path: 'images/plitvice.jpg', description: 'Plitvice description here', likes: 0, title: 'Plitvice Lakes National Park', showDescription: false  },
+    { id: 6, path: 'images/sacre-coeur.jpg', description: 'Sacre Coeur decription here', likes: 0, title: 'Sacré-Cœur Basilica', showDescription: false  },
+    { id: 7, path: 'images/wedding.jpg', description: 'Wedding description here', likes: 0, title: 'Wedding', showDescription: false  }
     
 ];
 
