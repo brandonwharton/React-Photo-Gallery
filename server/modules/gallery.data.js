0cf6c35540c6b091a2a9b39122a3fc02c39df634
Brandon Wharton <whartonbm@gmail.com>
2021-06-04 18:02:06 -0500

    Adjusted image key/values in gallery.data.js

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,12 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/acro.jpg', description: 'Acro description here', likes: 0, title: 'Acro Yoga' },
+    { id: 2, path: 'images/kauai.jpg', description: 'Kauai description here', likes: 0, title: 'Nepali Coast: Kauai, HI' },
+    { id: 3, path: 'images/mia.jpg', description: 'Mia description here', likes: 0, title: 'Mia' },
+    { id: 4, path: 'images/peaches.jpg', description: 'Peaches description here', likes: 0, title: 'Peaches' },
+    { id: 5, path: 'images/plitvice.jpg', description: 'Plitvice description here', likes: 0, title: 'Plitvice Lakes National Park' },
+    { id: 6, path: 'images/sacre-coeur.jpg', description: 'Sacre Coeur decription here', likes: 0, title: 'Sacré-Cœur Basilica' },
+    { id: 7, path: 'images/wedding.jpg', description: 'Wedding description here', likes: 0, title: 'Wedding' }
+    
 ];
 
 module.exports = galleryItems;
