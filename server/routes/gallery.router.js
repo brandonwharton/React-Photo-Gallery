ca1a9716be4e39f3b927022db6b698e945e07f21
Brandon Wharton <whartonbm@gmail.com>
2021-06-05 13:06:20 -0500

    Adjusted GET route to properly fetch data from DB

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -5,27 +5,41 @@ const pool = require('../modules/pool.js');
 
 // DO NOT MODIFY THIS FILE FOR BASE MODE
 
-// // PUT Route
-// router.put('/like/:id', (req, res) => {
-//     console.log(req.params);
-//     const galleryId = req.params.id;
-//     console.log(req.params.id);
+// PUT Route
+router.put('/like/:id', (req, res) => {
+    console.log(req.params);
+    const galleryId = req.params.id;
+    console.log(req.params.id);
     
-//     for(const galleryItem of galleryItems) {
-//         if(galleryItem.id == galleryId) {
-//             console.log('Before +=', galleryItem.likes);
+    for(const galleryItem of galleryItems) {
+        if(galleryItem.id == galleryId) {
+            console.log('Before +=', galleryItem.likes);
             
-//             galleryItem.likes += 1;
-//             console.log('After +=', galleryItem.likes);
+            galleryItem.likes += 1;
+            console.log('After +=', galleryItem.likes);
             
-//         }
-//     }
-//     res.sendStatus(200);
-// }); // END PUT Route
+        }
+    }
+    res.sendStatus(200);
+}); // END PUT Route
 
-// // GET Route
-// router.get('/', (req, res) => {
-//     res.send(galleryItems);
-// }); // END GET Route
+// GET Route
+router.get('/', (req, res) => {
+    // save a query to get all data from DB sorted by id
+    const queryText = `SELECT * FROM "images"`
+    // request data from DB
+    pool.query(queryText)
+    .then(response => {
+        console.log('Received data from DB', response.rows);
+        res.send(response.rows);
+    })
+    .catch(err => {
+        console.log('Server error with GET request to DB', err);
+        res.sendStatus(500)
+    })
+
+
+    // res.send(galleryItems);
+}); // END GET Route
 
 module.exports = router;
