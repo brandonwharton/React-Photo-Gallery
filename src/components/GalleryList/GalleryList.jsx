d8a1ff660a71cb0ba6bd917457439dc764aa7029
Brandon Wharton <whartonbm@gmail.com>
2021-06-04 19:02:34 -0500

    Created GalleryList component, imported css, started GalleryList function

---
+++ src/components/GalleryList/GalleryList.jsx
@@ -0,0 +1,11 @@
+import './GalleryList.css';
+
+function GalleryList ({galleryList, getGalleryList}) {
+
+    return (
+      <h2>Checking Gallery List</h2>  
+    );
+
+}
+
+export default GalleryList;
