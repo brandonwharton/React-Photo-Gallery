7fb331b583704d90068d307e90617b1998b0927d
Brandon Wharton <whartonbm@gmail.com>
2021-06-05 13:02:26 -0500

    Set up a pool.js file, imported pool into gallery.router.js

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,30 +1,31 @@
 const express = require('express');
 const router = express.Router();
-const galleryItems = require('../modules/gallery.data');
+// const galleryItems = require('../modules/gallery.data');
+const pool = require('../modules/pool.js');
 
 // DO NOT MODIFY THIS FILE FOR BASE MODE
 
-// PUT Route
-router.put('/like/:id', (req, res) => {
-    console.log(req.params);
-    const galleryId = req.params.id;
-    console.log(req.params.id);
+// // PUT Route
+// router.put('/like/:id', (req, res) => {
+//     console.log(req.params);
+//     const galleryId = req.params.id;
+//     console.log(req.params.id);
     
-    for(const galleryItem of galleryItems) {
-        if(galleryItem.id == galleryId) {
-            console.log('Before +=', galleryItem.likes);
+//     for(const galleryItem of galleryItems) {
+//         if(galleryItem.id == galleryId) {
+//             console.log('Before +=', galleryItem.likes);
             
-            galleryItem.likes += 1;
-            console.log('After +=', galleryItem.likes);
+//             galleryItem.likes += 1;
+//             console.log('After +=', galleryItem.likes);
             
-        }
-    }
-    res.sendStatus(200);
-}); // END PUT Route
+//         }
+//     }
+//     res.sendStatus(200);
+// }); // END PUT Route
 
-// GET Route
-router.get('/', (req, res) => {
-    res.send(galleryItems);
-}); // END GET Route
+// // GET Route
+// router.get('/', (req, res) => {
+//     res.send(galleryItems);
+// }); // END GET Route
 
 module.exports = router;
