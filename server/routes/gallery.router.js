5887bf95a4636301e3a350f60a635d015f39f07a
Brandon Wharton <whartonbm@gmail.com>
2021-06-05 13:14:29 -0500

    Updated PUT request to communicate with DB

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -7,39 +7,36 @@ const pool = require('../modules/pool.js');
 
 // PUT Route
 router.put('/like/:id', (req, res) => {
-    console.log(req.params);
     const galleryId = req.params.id;
-    console.log(req.params.id);
-    
-    for(const galleryItem of galleryItems) {
-        if(galleryItem.id == galleryId) {
-            console.log('Before +=', galleryItem.likes);
-            
-            galleryItem.likes += 1;
-            console.log('After +=', galleryItem.likes);
-            
-        }
-    }
-    res.sendStatus(200);
+    // save a query to add a like to an image in the DB with a sanitized galleryId
+    const queryText = `UPDATE "images" SET "likes" = likes + 1 WHERE "id" = $1;`
+    // request a like increase to DB
+    pool.query(queryText, [galleryId])
+        .then(response => {
+            // send back an OK if succesful
+            res.sendStatus(200);
+        })
+        .catch(err => {
+            console.log('Server error with PUT request to DB', err);
+            res.sendStatus(500);
+        });
 }); // END PUT Route
+
 
 // GET Route
 router.get('/', (req, res) => {
     // save a query to get all data from DB sorted by id
-    const queryText = `SELECT * FROM "images"`
+    const queryText = `SELECT * FROM "images" ORDER BY "id" ASC`
     // request data from DB
     pool.query(queryText)
-    .then(response => {
-        console.log('Received data from DB', response.rows);
-        res.send(response.rows);
-    })
-    .catch(err => {
-        console.log('Server error with GET request to DB', err);
-        res.sendStatus(500)
-    })
-
-
-    // res.send(galleryItems);
+        .then(response => {
+            console.log('Received data from DB', response.rows);
+            res.send(response.rows);
+        })
+        .catch(err => {
+            console.log('Server error with GET request to DB', err);
+            res.sendStatus(500)
+        });
 }); // END GET Route
 
 module.exports = router;
